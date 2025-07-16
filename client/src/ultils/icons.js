161a0124d1f86470ffc14852a4e988d3a578dae8
Congpes <congpes0705@gmail.com>
2025-07-16 23:27:17 +0700

    Công Fe

---
+++ client/src/ultils/icons.js
@@ -0,0 +1,47 @@
+import { CiCirclePlus } from 'react-icons/ci'
+import { GrNext,GrFormNextLink, GrFormPreviousLink } from "react-icons/gr"
+import { IoLocationOutline } from "react-icons/io5";
+import { MdAttachMoney, MdLogout } from "react-icons/md";
+import { RiCrop2Line } from "react-icons/ri";
+import { PiHouseLine, PiNotePencilFill } from "react-icons/pi";
+import { FiSearch } from "react-icons/fi";
+import { RxStarFilled } from "react-icons/rx";
+import { GoHeart, GoHeartFill } from "react-icons/go";
+import { BsBookmarkStarFill, BsCameraFill, BsDot } from "react-icons/bs";
+import { LuNotebook, LuClock8 } from "react-icons/lu";
+import { FaRegUserCircle, FaCaretDown } from "react-icons/fa";
+import { ImBin } from "react-icons/im";
+import { FaLocationDot, FaPhoneVolume } from "react-icons/fa6";
+import { HiHashtag } from "react-icons/hi";
+import { SiZalo } from "react-icons/si";
+
+const icons = {
+    CiCirclePlus,
+    GrNext,
+    IoLocationOutline,
+    MdAttachMoney,
+    RiCrop2Line,
+    PiHouseLine,
+    FiSearch,
+    RxStarFilled,
+    GoHeart,
+    GoHeartFill,
+    BsBookmarkStarFill,
+    GrFormNextLink,
+    GrFormPreviousLink,
+    PiNotePencilFill,
+    LuNotebook,
+    FaRegUserCircle,
+    MdLogout,
+    FaCaretDown,
+    BsCameraFill,
+    ImBin,
+    FaLocationDot,
+    LuClock8,
+    HiHashtag,
+    BsDot,
+    FaPhoneVolume,
+    SiZalo
+}
+
+export default icons
